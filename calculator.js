7819e9cafbb9e6a28588fe71f0ef27dc60c526ee
andre <andre.costa@tpv-tech.com>
2020-03-30 13:01:00 -0400

    add circleci

---
+++ calculator.js
@@ -0,0 +1,19 @@
+class Calculator {
+  static add (x, y) {
+    return x + y
+  }
+
+  static subtract (x, y) {
+    return x - y
+  }
+
+  static multiplication (x, y) {
+    return x * y
+  }
+
+  static division (x, y) {
+    return x / y
+  }
+}
+
+module.exports = { Calculator: Calculator }
---
+++ test/calculator.test.js
@@ -0,0 +1,23 @@
+const { Calculator } = require('../calculator')
+
+describe('Calculator Test', () => {
+  it('Add method', () => {
+    const [firstInput, secondInput, output] = [5, 5, 10]
+    expect(Calculator.add(firstInput, secondInput)).toEqual(output)
+  })
+
+  it('Subtract method', () => {
+    const [firstInput, secondInput, output] = [5, 5, 0]
+    expect(Calculator.subtract(firstInput, secondInput)).toEqual(output)
+  })
+
+  it('Multiplication method', () => {
+    const [firstInput, secondInput, output] = [5, 5, 25]
+    expect(Calculator.multiplication(firstInput, secondInput)).toEqual(output)
+  })
+
+  it('Division method', () => {
+    const [firstInput, secondInput, output] = [5, 5, 1]
+    expect(Calculator.division(firstInput, secondInput)).toEqual(output)
+  })
+})
